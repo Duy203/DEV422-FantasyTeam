35454182ff6d250a5456ce92f07d2f692992fdd8
Duy203 <duy.bao.nguyen@bellevuecollege.edu>
2025-12-08 15:32:58 -0800

    added middle ware folder

---
+++ DEV422-FantasyTeam/Middleware/APIKeyMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+namespace DEV422_FantasyTeam.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEY_HEADER = "X-API-KEY";
+        private readonly string _apiKey;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration config)
+        {
+            _next = next;
+            _apiKey = config["ApiKey"];
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(!context.Request.Headers.TryGetValue(APIKEY_HEADER,out var providedKey))
+            {
+                context.Response.StatusCode = 401; // Unauthorized
+                await context.Response.WriteAsync("API Key is missing.");
+                return;
+
+            }
+
+            if(!_apiKey.Equals(providedKey))
+            {
+                context.Response.StatusCode = 403; // Forbidden
+                await context.Response.WriteAsync("Invalid API Key.");
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
